582865407d8ae6e16db766fe7f8f8fd195f6e0a6
pantsuX <d.rozenbergs@outlook.com>
2023-03-14 11:03:16 +0000

    111

--- python/python game/game.py
+++ python/python game/game.py
@@ -4,5 +4,9 @@ pygame.init()
 screen = pygame.display.set_mode((600,400))
 
 while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            pygame.quit()
     
     pygame.display.update()
